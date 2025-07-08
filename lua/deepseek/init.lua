1b473d4e7f8aeee4887950cfec60c129618d5b73
xunshichentuo <chentuo@sprintray.cn>
2025-07-08 14:30:26 +0800

    add input window and buff

--- lua/deepseek/init.lua
+++ lua/deepseek/init.lua
@@ -3,16 +3,23 @@ local M = {}
 -- 默认配置
 local defaults = {
 	enabled = true,
-	-- 添加窗口相关配置
 	window = {
 		width = 80,
 		height = 20,
+		split_ratio = 0.4,
 	},
-
-	-- 快捷键
 	keymaps = {
 		open_chat = "<leader>dc",
+		submit = "<C-Enter>",
 	},
+}
+
+-- 保存窗口和缓冲区引用
+local state = {
+	input_win = nil,
+	output_win = nil,
+	input_buf = nil,
+	output_buf = nil,
 }
 
 function M.setup(opts)
@@ -35,65 +42,167 @@ end
 -- 设置快捷键函数
 function M.setup_keymaps()
 	vim.keymap.set("n", M.config.keymaps.open_chat, function()
-		M.open_chat_window()
+		M.open_chat_ui()
 	end, { desc = "打开 DeepSeek 聊天窗口" })
 end
 
 -- 打开聊天窗口
-function M.open_chat_window()
-	-- 创建一个新的浮动窗口
-	local chat_buf = vim.api.nvim_create_buf(false, true)
+function M.open_chat_ui()
+	-- 如果窗口已经存在，则聚焦到输入窗口
+	if state.input_win and vim.api.nvim_win_is_valid(state.input_win) then
+		vim.api.nvim_set_current_win(state.input_win)
+		return
+	end
 
-	-- 设置窗口选项
-	local opts = {
+	-- 创建两个缓冲区
+	state.input_buf = vim.api.nvim_create_buf(false, true)
+	state.output_buf = vim.api.nvim_create_buf(false, true)
+
+	local total_height = M.config.window.height
+	local input_height = math.floor(total_height * M.config.window.split_ratio)
+	local output_height = total_height - input_height - 1
+
+	state.input_win = vim.api.nvim_open_win(state.input_buf, true, {
+		width = M.config.window.width,
+		relative = "editor",
+		height = input_height,
+		col = (vim.o.columns - M.config.window.width) / 2,
+		row = (vim.o.lines - total_height) / 2,
+		border = "single",
+		title = "输入区（按ESC关闭, Ctrl+Enter 提交）",
+		title_pos = "center",
+	})
+
+	state.output_win = vim.api.nvim_open_win(state.output_buf, true, {
 		relative = "editor",
 		width = M.config.window.width,
-		height = M.config.window.height,
+		height = output_height,
 		col = (vim.o.columns - M.config.window.width) / 2,
-		row = (vim.o.lines - M.config.window.height) / 2,
-		style = "minimal",
+		row = (vim.o.lines - total_height) / 2 + input_height + 1,
 		border = "single",
-	}
+		title = "输出区",
+		title_pos = "center",
+	})
 
-	-- 创建窗口
-	local win = vim.api.nvim_open_win(chat_buf, true, opts)
+	vim.api.nvim_win_set_option(state.input_win, "winhl", "Normal:Normal,FloatBorder:FloatBorder")
+	vim.api.nvim_win_set_option(state.output_win, "winhl", "Normal:Normal,FloatBorder:FloatBorder")
 
-	-- 配置窗口
-	vim.api.nvim_buf_set_option(chat_buf, "filetype", "markdown")
-	vim.api.nvim_buf_set_option(chat_buf, "modifiable", false)
-	vim.api.nvim_buf_set_option(chat_buf, "number", false)
-	vim.api.nvim_buf_set_option(chat_buf, "relativenumber", false)
-	vim.api.nvim_buf_set_option(chat_buf, "wrap", true)
+	M.setup_buffers()
+
+	M.setup_autocmds()
+end
+
+function M.setup_buffers()
+	-- 输入缓冲区设置
+	vim.api.nvim_buf_set_option(state.input_buf, "filetype", "markdown")
+	vim.api.nvim_buf_set_option(state.input_buf, "modifiable", true)
+	vim.api.nvim_buf_set_option(state.input_buf, "buftype", "")
+	vim.api.nvim_buf_set_option(state.input_buf, "number", false)
+	vim.api.nvim_buf_set_option(state.input_buf, "relativenumber", false)
+	vim.api.nvim_buf_set_option(state.input_buf, "wrap", true)
+
+	-- 输出缓冲区设置
+	vim.api.nvim_buf_set_option(state.output_buf, "filetype", "markdown")
+	vim.api.nvim_buf_set_option(state.output_buf, "modifiable", true)
+	vim.api.nvim_buf_set_option(state.input_buf, "buftype", "nofile")
+	vim.api.nvim_buf_set_option(state.output_buf, "number", false)
+	vim.api.nvim_buf_set_option(state.output_buf, "relativenumber", false)
+	vim.api.nvim_buf_set_option(state.output_buf, "wrap", true)
+
+	-- 设置初始内容
+	vim.api.nvim_buf_set_lines(state.input_buf, 0, -1, false, { "在此输入您的问题..." })
+	vim.api.nvim_buf_set_lines(state.output_buf, 0, -1, false, { "等待您的问题..." })
+
+	vim.api.nvim_buf_set_option(state.output_buf, "modifiable", false)
+	vim.api.nvim_buf_set_option(state.output_buf, "readonly", true)
 
 	-- 输入窗口映射
 	vim.api.nvim_buf_set_keymap(
-		chat_buf,
+		state.input_buf,
 		"n",
 		"<Esc>",
-		"<cmd>q!<CR>",
-		{ noremap = true, silent = true, nowait = true, desc = "关闭 DeepSeek 聊天窗口" }
+		"<cmd>lua require('deepseek').close_windows()<CR>",
+		{ noremap = true, silent = true, nowait = true, desc = "关闭聊天窗口" }
 	)
 	vim.api.nvim_buf_set_keymap(
-		chat_buf,
-		"n",
-		"q",
-		"<cmd>q!<CR>",
-		{ noremap = true, silent = true, nowait = true, desc = "关闭 DeepSeek 聊天窗口" }
+		state.input_buf,
+		"i",
+		"<C-Enter>",
+		"<cmd>lua require('deepseek').submit_input()<CR>",
+		{ noremap = true, silent = true, nowait = true, desc = "提交输入" }
 	)
+end
 
+function M.setup_autocmds()
 	vim.api.nvim_create_autocmd("BufWipeout", {
-		buffer = chat_buf,
+		buffer = state.input_buf,
 		callback = function()
-			if vim.api.nvim_win_is_valid(win) then
-				vim.api.nvim_win_close(win, true)
+			if state.input_win and vim.api.nvim_win_is_valid(state.input_win) then
+				vim.api.nvim_win_close(state.input_win, true)
+			end
+			if state.output_win and vim.api.nvim_win_is_valid(state.output_win) then
+				vim.api.nvim_win_close(state.output_win, true)
 			end
 		end,
 	})
+end
 
-	return {
-		win = win,
-		buf = chat_buf,
-	}
+function M.close_windows()
+	vim.notify("正在关闭DeepSeek窗口...", vim.log.levels.INFO)
+	local current_win = vim.api.nvim_get_current_win()
+	if current_win == state.input_win or current_win == state.output_win then
+		vim.cmd("wincmd p")
+	end
+
+	if state.input_win and vim.api.nvim_win_is_valid(state.input_win) then
+		vim.api.nvim_win_close(state.input_win, true)
+	end
+	if state.output_win and vim.api.nvim_win_is_valid(state.output_win) then
+		vim.api.nvim_win_close(state.output_win, true)
+	end
+
+	vim.defer_fn(function()
+		if state.input_buf and vim.api.nvim_buf_is_valid(state.input_buf) then
+			pcall(vim.api.nvim_buf_delete, state.input_buf, { force = true })
+		end
+		if state.output_buf and vim.api.nvim_buf_is_valid(state.output_buf) then
+			pcall(vim.api.nvim_buf_delete, state.output_buf, { force = true })
+		end
+
+		state = {
+			input_win = nil,
+			output_win = nil,
+			input_buf = nil,
+			output_buf = nil,
+		}
+		vim.notify("DeepSeek窗口已关闭", vim.log.levels.INFO)
+	end, 50)
+end
+
+function M.submit_input()
+	--临时允许输出缓冲区修改
+	vim.api.nvim_buf_set_option(state.output_buf, "modifiable", true)
+	vim.api.nvim_buf_set_option(state.output_buf, "readonly", false)
+
+	local input_lines = vim.api.nvim_buf_get_lines(state.input_buf, 0, -1, false)
+
+	-- 模拟回复
+	vim.api.nvim_buf_set_lines(state.output_buf, 0, -1, false, {
+		"您输入了：",
+		"",
+		table.concat(input_lines, "\n"),
+		"",
+		"------------------",
+		"这是模拟回复 - 实际使用时这里会是 API 返回的内容",
+		"当前时间：" .. os.date("%Y-%m-%d %H:%M:%S"),
+	})
+
+	vim.api.nvim_buf_set_option(state.output_buf, "modifiable", false)
+	vim.api.nvim_buf_set_option(state.output_buf, "readonly", true)
+
+	--清空输入区
+	vim.api.nvim_buf_set_lines(state.input_buf, 0, -1, false, { "" })
+	vim.api.nvim_win_set_cursor(state.input_win, { 1, 0 })
 end
 
 return M
